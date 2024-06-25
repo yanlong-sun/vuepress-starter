f5270b480664ae2fcaf25615f0ca46302f84dc83
yanlong-sun <sunyanlong712@gmail.com>
2024-06-26 00:01:38 +0800

    vue-press

---
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,19 @@
+import * as clientConfig0 from '/Users/yanlongsun/s/blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-active-header-links@2.0.0-rc.37_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@_q3xqfzowemxgfra7nsfadx2ogq/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from '/Users/yanlongsun/s/blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-back-to-top@2.0.0-rc.37_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@2.0.0-rc_jbxtogwerqeqz3klr7fzhm6pdm/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from '/Users/yanlongsun/s/blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-copy-code@2.0.0-rc.37_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@2.0.0-rc.7_mtaqn4fwp7fmizvjnbyvgw4eou/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from '/Users/yanlongsun/s/blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-medium-zoom@2.0.0-rc.37_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@2.0.0-rc_gz6rd3c3k5brprlfabnchxhtba/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig4 from '/Users/yanlongsun/s/blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-nprogress@2.0.0-rc.37_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@2.0.0-rc.7_zxfjv7yxuiodykosrpsxvtldoy/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig5 from '/Users/yanlongsun/s/blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-theme-data@2.0.0-rc.37_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@2.0.0-rc._rqnnvnprrcqg5ydgj3obrtggpe/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig6 from '/Users/yanlongsun/s/blog/vuepress-starter/node_modules/.pnpm/@vuepress+theme-default@2.0.0-rc.37_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@2.0.0-rc.7_@t_vbp4bazbmftoz54upwsmcctrlm/node_modules/@vuepress/theme-default/lib/client/config.js'
+import * as clientConfig7 from '/Users/yanlongsun/s/blog/vuepress-starter/docs/.vuepress/client.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+  clientConfig7,
+].map((m) => m.default).filter(Boolean)
